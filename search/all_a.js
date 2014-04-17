0551d82d1a4c0bd08851ccfb8592cea1e78d6d6c
YoNuTzZ <ionut.grt@gmail.com>
2014-04-18 00:40:14 +0300

    Add reffs to consants

--- search/all_a.js
+++ search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['name',['name',['../structPlayer.html#a4cefb7a127807dd94ec8fa3828bad582',1,'Player::name()'],['../structTeam.html#abeda6f24746e08e7b4cbce2b732e91e2',1,'Team::name()']]],
+  ['name',['name',['../structPlayer.html#a4cefb7a127807dd94ec8fa3828bad582',1,'Player']]],
   ['no_5ferror',['NO_ERROR',['../errors_8h.html#a4db9ee29f2ff83c71567c12f6bfbf28cabf350750d0d4fabd8954c0f1e9bbae94',1,'errors.h']]],
   ['not_5ffound',['NOT_FOUND',['../errors_8h.html#a4db9ee29f2ff83c71567c12f6bfbf28cacdaa2919bac56fe1090eb3dbb9526472',1,'errors.h']]],
   ['numberplayers',['numberPlayers',['../structGame.html#ad99c0390066c13b0ca6b89f8a89c54b5',1,'Game']]]
